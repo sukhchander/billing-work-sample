1706c70878269c639a1e9db0f7ee45dbb18683a1
sukhchander <sukhchander@gmail.com>
2016-01-03 23:24:09 -0500

    rename namespace. cleanup markup. add preloaders for charts.

--- app/assets/javascripts/application/init.js
+++ app/assets/javascripts/application/init.js
@@ -1,31 +1,18 @@
-var Rocketboard = function() {
+var Application = function() {
 
     $(function() {
-        toggleSettings();
         navToggleLeft();
         navToggleSub();
     });
 
-    var toggleSettings = function() {
-        $('.config-link').click(function() {
-            if ($(this).hasClass('open')) {
-                $('#config').animate({
-                    "right": "-205px"
-                }, 150);
-                $(this).removeClass('open').addClass('closed');
-            } else {
-                $("#config").animate({
-                    "right": "0px"
-                }, 150);
-                $(this).removeClass('closed').addClass('open');
-            }
-        });
-    };
-
     var navToggleLeft = function() {
         $('#toggle-left').on('click', function() {
             var bodyEl = $('#main-wrapper');
-            ($(window).width() > 767) ? $(bodyEl).toggleClass('sidebar-mini'): $(bodyEl).toggleClass('sidebar-opened');
+            if ($(window).width() > 767) {
+              $(bodyEl).toggleClass('sidebar-mini');
+            } else {
+              $(bodyEl).toggleClass('sidebar-opened');
+            }
         });
     };
 
@@ -37,17 +24,19 @@ var Rocketboard = function() {
         });
     };
 
-    var profileToggle = function() {
-        $('#toggle-profile').click(function() {
-            $('.sidebar-profile').slideToggle();
-        });
+    var spinStart = function(spinOn) {
+        var spinFull = $('<div class="preloader"><div class="iconWrapper"><i class="fa fa-circle-o-notch fa-spin fa-3x"></i></div></div>');
+        var spinInner = $('<div class="preloader preloader-inner"><div class="iconWrapper"><i class="fa fa-circle-o-notch fa-spin fa-3x"></i></div></div>');
+        if (spinOn === undefined) {
+            $('body').prepend(spinFull);
+        } else {
+            $(spinOn).prepend(spinInner);
+        };
     };
 
-
-    function cb(start, end) {
-      $('#reportrange span').html(start.format('YYYY-MM-DD') + ' - ' + end.format('YYYY-MM-DD'));
-    }
-    cb(moment().subtract(29, 'days'), moment());
+    var spinStop = function() {
+        $('.preloader').fadeOut().remove();
+    };
 
     var dateRangePicker = function() {
         var today = new Date();
@@ -79,129 +68,72 @@ var Rocketboard = function() {
         });
     };
 
-    var tooltips = function() {
-        $('.tooltip-wrapper').tooltip({
-            selector: "[data-toggle=tooltip]",
-            container: "body"
-        })
-    };
+    var drawDonutChart = function (url, container) {
+      var element = document.getElementById(container);
+      spinStart($(element).parent());
 
-    var sliders = function() {
-        $('.slider-span').slider()
-    };
-
-    var chartJs = function() {
-
-        var areaChartData = {};
-        var drawAreaChart = function (url) {
-          $.ajax({
-              url: url,
-              method: 'GET',
-              dataType: 'json',
-              success: function (d) {
-
-                areaChartData = {
-                  labels: d.xAxis,
-                  datasets: [{
-                    //fillColor: 'rgba(26,188,156,0.5)',
-                    //strokeColor: 'rgba(26,188,156,1)',
-                    //pointColor: 'rgba(220,220,220,1)',
-                    fillColor: "rgba(151,187,205,0.2)",
-                    strokeColor: "rgba(151,187,205,1)",
-                    pointColor: "rgba(151,187,205,1)",
-                    pointStrokeColor: '#fff',
-                    pointHighlightFill: '#fff',
-                    pointHighlightStroke: 'rgba(220,220,220,1)',
-                    data: d.zAxis
-                  }]
-                };
-                //console.log(areaChartData.labels);
-                //console.log(areaChartData.datasets[0]["data"]);
-
-                var ctx1 = document.getElementById("canvas1").getContext("2d");
-                window.myLine = new Chart(ctx1).Line(areaChartData, {
-                  responsive: true,
-                  scaleLabel : "<%= accounting.formatMoney(value) %>",
-                  tooltipTemplate: "<%= accounting.formatMoney(value) %>"
-                });
-
-            }
-          });
-        };
-
-        var doughnutChartData = [];
-        var drawDoughnutChart = function (url) {
-          $.ajax({
-            url: url,
-            method: 'GET',
-            dataType: 'json',
-            success: function (d) {
-
-              doughnutChartData = d.data;
-
-              var ctx3 = document.getElementById("doughnut-chart-area").getContext("2d");
-              window.myDoughnut = new Chart(ctx3).Doughnut(doughnutChartData, {
-                responsive: true
-              });
-            }
-          });
-        };
-
-        window.onload = function() {
-
-          drawAreaChart("/api/v1/data/chart/aggregate");
-
-          drawDoughnutChart("/api/v1/data/chart/breakdown_product");
-
-        };
-
-    };
-
-    var drawAreaChart = function(url,selector) {
-      var areaChartData = {};
+      var data = [];
       $.ajax({
-          url: url,
-          method: 'GET',
-          dataType: 'json',
-          success: function (d) {
+        url: url,
+        method: 'GET',
+        dataType: 'json',
+        beforeSend: function () {
+        },
+        success: function (response) {
 
-            areaChartData = {
-              labels: d.xAxis,
-              datasets: [{
-                //fillColor: 'rgba(26,188,156,0.5)',
-                //strokeColor: 'rgba(26,188,156,1)',
-                //pointColor: 'rgba(220,220,220,1)',
-                fillColor: "rgba(151,187,205,0.2)",
-                strokeColor: "rgba(151,187,205,1)",
-                pointColor: "rgba(151,187,205,1)",
-                pointStrokeColor: '#fff',
-                pointHighlightFill: '#fff',
-                pointHighlightStroke: 'rgba(220,220,220,1)',
-                data: d.zAxis
-              }]
-            };
-            //console.log(areaChartData.labels);
-            //console.log(areaChartData.datasets[0]["data"]);
+          spinStop($(element).parent());
 
-            var context = document.getElementById(selector).getContext("2d");
-            window.myLine = new Chart(context).Line(areaChartData, {
-              responsive: true,
-              scaleLabel : "<%= accounting.formatMoney(value) %>",
-              tooltipTemplate: "<%= accounting.formatMoney(value) %>"
-            });
+          data = response.data;
+          var context = document.getElementById(container).getContext("2d");
+          window.donutChart = new Chart(context).Doughnut(data, {
+            responsive: true
+          });
+        }
+      });
+    };
 
+    var drawAreaChart = function(url, container) {
+      var element = document.getElementById(container);
+      spinStart($(element).parent());
+
+      var data = {};
+      $.ajax({
+        url: url,
+        method: 'GET',
+        dataType: 'json',
+        beforeSend: function () {
+        },
+        success: function (response) {
+
+          spinStop($(element).parent());
+
+          data = {
+            labels: response.xAxis,
+            datasets: [{
+              fillColor: "rgba(151,187,205,0.2)",
+              strokeColor: "rgba(151,187,205,1)",
+              pointColor: "rgba(151,187,205,1)",
+              pointStrokeColor: '#fff',
+              pointHighlightFill: '#fff',
+              pointHighlightStroke: 'rgba(220,220,220,1)',
+              data: response.zAxis
+            }]
+          };
+
+          var context = document.getElementById(container).getContext("2d");
+          window.areaChart = new Chart(context).Line(data, {
+            responsive: true,
+            scaleLabel : "<%= accounting.formatMoney(value) %>",
+            tooltipTemplate: "<%= accounting.formatMoney(value) %>"
+          });
         }
       });
     };
 
     return {
         dateRangePicker: dateRangePicker,
-        chartJs: chartJs,
+        drawDonutChart: drawDonutChart,
         drawAreaChart: drawAreaChart
     };
 
 }();
-
-$(window).resize(function() {
-    Rocketboard.chartJs();
-});
